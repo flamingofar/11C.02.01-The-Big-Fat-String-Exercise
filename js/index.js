17b2438ca534f85ef1700c67490bbc99235a731f
malte <malt0533@stud.kea.dk>
2022-02-05 17:04:57 +0100

    Initial commit and first option done

---
+++ js/index.js
@@ -0,0 +1,47 @@
+/** @format */
+
+console.log("Hi");
+
+const input = document.querySelector(".input");
+const output = document.querySelector(".output");
+const select = document.querySelector("select");
+const btn = document.querySelector("button");
+
+let name;
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+	input.value = "";
+	btn.addEventListener("click", computeInput);
+}
+
+function computeInput() {
+	let selectValue = select.value;
+	switch (selectValue) {
+		case "0":
+			name = input.value.toLowerCase();
+			console.log(`${name.substring(0, 1).toUpperCase()}${name.substring(1)}`);
+			break;
+		case "1":
+			console.log("1");
+			break;
+		case "2":
+			console.log("2");
+			break;
+		case "3":
+			console.log("3");
+			break;
+		case "4":
+			console.log("4");
+			break;
+		case "5":
+			console.log("5");
+			break;
+		case "6":
+			console.log("6");
+			break;
+		case "7":
+			console.log("7");
+			break;
+	}
+}
